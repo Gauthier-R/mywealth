50d80a3813bb69a85fb5bf04248d71b981913ef7
Gauthier-R <gauthier.raimbault@gmail.com>
2026-09-19 17:02:27 +0200

    chore: restructuration à la racine et mise à jour du gitignore

--- Flutter/Diners/dinners_app/dinners_app/android/app/src/main/kotlin/fr/dinners/app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package fr.dinners.app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- Python/Projet Scrum/main.py
+++
@@ -1,6 +0,0 @@
-from interface import Application
-
-if __name__ == "__main__" :
-    app = Application()
-    app.interface()
-    app.run_app()
